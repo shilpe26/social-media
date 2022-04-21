fade189a5a9a9fe37bdbc5dfb3e13bde39b54f96
SHILPE SAXENA <shilpecsaxena9098@gmail.com>
2022-04-21 11:18:08 +0530

    add: Addes Static files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
-
+import Mockman from "mockman-js";
+import { Routes, Route } from "./react-router-dom";
+import { Login, Signup, Home } from "./pages/Pages";
+import { Navbar, Footer, PageNotFound } from "./components/Components";
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<div>
+				<Navbar />
+			</div>
+			<div className="main-style grow">
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/signup" element={<Signup />} />
+					<Route path="/mock" element={<Mockman />} />
+					<Route path="*" element={<PageNotFound />} />
+				</Routes>
+			</div>
+			<div>
+				<Footer />
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import { makeServer } from "./server";
 
 // Call make Server
 makeServer();
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>
 );
